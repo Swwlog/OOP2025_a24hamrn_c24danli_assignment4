03c00ecdc8a2a3aec8edf03aa95aa77a2de1afac
hampu <hampu@10.111.34.42>
2025-03-13 16:03:00 +0100

    Cheess

--- OOP2025_a24hamrh_a25danli_assignment4/src/Main/ChessBoard.java
+++ OOP2025_a24hamrh_a25danli_assignment4/src/Main/ChessBoard.java
@@ -1,17 +1,33 @@
 package Main;
 
-public class ChessBoard {
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ChessBoard extends JPanel{
 	private int numRows=8;
 	private int numCollumns=8;
 	private int squareSize=100;
 	boolean isWhite=true;
 	
-	public ChessBoard() {
+	public void paintComponent(Graphics g) {
+		//super.paintComponent(g);
+	
+		/*
 		for(int i=0; i < numRows; i++) {
+			System.out.println(i);
 			for(int j=0; j<numCollumns; j++) {
-				//drawSquare()
+				
+				//g.drawRect(i,j ,squareSize , squareSize);
+				super.paintComponent(g);
+				g.setColor(Color.BLACK);
+				g.fillRect(i, j, squareSize, squareSize);
 			}
 		}
+		*/
+		
+	
 	}
-
+	
 }
--- OOP2025_a24hamrh_a25danli_assignment4/src/Main/GameScreen.java
+++ OOP2025_a24hamrh_a25danli_assignment4/src/Main/GameScreen.java
@@ -7,17 +7,32 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
-
+	private static ChessBoard chessBoard;
 	private int width = 800;
 	private int hight = 800;
 
 	public GameScreen() {
 		setPreferredSize(new Dimension(width, hight));
 		setBackground(Color.darkGray);
+		// add(chessBoard = new ChessBoard());
+
+		revalidate();
+		repaint();
 	}
+
 	@Override
-public void paintComponent(Graphics g) {
-	g.drawRect(hight, hight, width, hight);
-}
+	public void paintComponent(Graphics g) {
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if ((x + y) % 2 == 0) {
+					g.setColor(Color.WHITE);
+				} else {
+					g.setColor(Color.BLACK);
+				}
+
+				g.fillRect(x * 100, y * 100, 100, 100);
+			}
+		}
+	}
 
 }
