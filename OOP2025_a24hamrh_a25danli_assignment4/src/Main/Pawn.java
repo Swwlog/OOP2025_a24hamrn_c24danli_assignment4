6bb25755a6c09a39414e9a3046bf4ddf56bf0b43
Brukare <Brukare@DESKTOP-VNL6QRN.lan>
2025-03-26 14:20:10 +0100

    pawn bug fix 2

--- OOP2025_a24hamrh_a25danli_assignment4/src/Main/Pawn.java
+++ OOP2025_a24hamrh_a25danli_assignment4/src/Main/Pawn.java
@@ -9,7 +9,7 @@ public class Pawn extends Piece {
 	public Pawn(int collumn, int row, Boolean isWhite, int widht, int hight) {
 		super(collumn, row, isWhite, widht, hight);
 		name = "Pawn";
-
+		
 		// check what color of sprite to add Sprite
 		if (isWhite) {
 			setSprite(getSprite("/piece/white-Pawn"));
@@ -38,7 +38,7 @@ public class Pawn extends Piece {
 		else if ((getIsWhite()==false && ((this.getRow()-newRow)==-1)&& this.getCollumn()==newCol)&&squareOccupiedOppositeColor(newCol, newRow, pieceList)==false
 			|| (getIsWhite()==false && squareOccupiedOppositeColor(newCol, newRow, pieceList) && ((this.getRow()-newRow)==-1)&& (Math.abs(getCollumn()-newCol)==1))
 			||getHasMoved()==false && ((getIsWhite()==false && ((this.getRow()-newRow)==-2)&& this.getCollumn()==newCol))&&squareOccupiedOppositeColor(newCol, newRow, pieceList)==false
-			&&!targetBlockedStraitLines(newCol,newRow,pieceList)) 
+			&&!targetBlockedStraitLines(newCol,newRow,pieceList)) // test
 		{
 			return true;
 		}
